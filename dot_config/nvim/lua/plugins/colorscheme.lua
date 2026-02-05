65d71e26b1818bccca4de4905c41d0129d6bc452
Kevin Morris <kevinmorrisone@icloud.com>
2026-02-05 12:45:51 -0500

    nuke

---
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,20 @@
+--return {
+--  { "ellisonleao/gruvbox.nvim" }, --the theme plugin
+--  { "LazyVim/LazyVim", opts = { colorscheme = "gruvbox", }, }, -- Set as default colorscheme
+--}
+
+--return {
+--  { "shaunsingh/nord.nvim" }, -- The theme plugin itself
+--  { "LazyVim/LazyVim", opts = { colorscheme = "nord", }, },
+--}
+
+return {
+  { "sainnhe/gruvbox-material" }, -- The theme plugin itself
+  { "LazyVim/LazyVim", opts = { colorscheme = "gruvbox-material", }, },
+}
+
+--return {
+--  { "rebelot/kanagawa.nvim" }, -- The theme plugin itself
+--  { "LazyVim/LazyVim", opts = { colorscheme = "kanagawa", }, },
+--}
+
---
+++ dot_config/nvim/lua/plugins/disabled.lua
@@ -0,0 +1,5 @@
+return {
+  { "mini-nvim/mini.pairs", enabled = false },
+  { "folke/noice.nvim", enabled = false },
+  { "rcarriga/nvim-notify", enabled = false },
+}
